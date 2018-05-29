00786f63ddfa428041c23c672ed8f17404e222a3
Honie Moon <honiemoon@gmail.com>
2018-05-28 20:22:27 -0700

    update map 1 and index.rmd

--- official_map.R
+++ official_map.R
@@ -1,44 +1,43 @@
 # Load necessary libraries into R
+library(plotly)
 library(ggplot2)
 library(dplyr)
-library(tidyr)
-library(maps)
-library(mapdata)
-library(ggmap)
-library(plotly)
-library(lintr)
-  
+packageVersion("plotly")
+
 # Load the raw data
-  raw_data <- read.csv("data/spm_2016.csv", stringsAsFactors = FALSE)
-  official_data <- read.csv("data/official-percentage.csv", 
-                            stringsAsFactors = FALSE)
-  
-  total_data <- official_data %>%
-    mutate(raw_data$State) 
-  
-  colnames(total_data)[4] <- "state"
-  
+raw_data <- read.csv("data/spm_2016.csv", stringsAsFactors = FALSE)
+official_data <- read.csv("data/official-percentage.csv",
+  stringsAsFactors = FALSE
+)
+
+total_data <- official_data %>%
+  mutate(raw_data$State)
+
+colnames(total_data)[4] <- "state"
+
 # Map Projections
-  g <- list(
-    scope = "usa",
-    projection = list(type = "albers usa"),
-    showland = TRUE,
-    landcolor = toRGB("gray85"),
-    subunitwidth = 1,
-    countrywidth = 1,
-    subunitcolor = toRGB("white"),
-    countrycolor = toRGB("white")
-  )
-  
-  total_data$hover <- with(total_data, paste(
-    state, "<br>",
-    "Official Percent Estimate: ", official, "<br>"
-  ))
-  
-  plot <- plot_geo(
-    total_data, locationmode = "USA-states") %>%
-    add_trace(
-      x = total_data$long, y = total_data$lat, text = total_data$hover, 
-      size = total_data$official, color = "grey") %>%
-    layout(title = "Official Percent Estimate", geo = g)
-  
+g <- list(
+  scope = "usa",
+  projection = list(type = "albers usa"),
+  showland = TRUE,
+  landcolor = toRGB("gray85"),
+  subunitwidth = 1,
+  countrywidth = 1,
+  subunitcolor = toRGB("white"),
+  countrycolor = toRGB("white")
+)
+
+total_data$hover <- with(total_data, paste(
+  state, "<br>",
+  "Official Percent Estimate: ", official, "<br>"
+))
+
+plot <- plot_geo(
+  total_data,
+  locationmode = "USA-states", sizes = c(1, 250)
+) %>%
+  add_markers(
+    x = total_data$long, y = total_data$lat, text = total_data$hover,
+    locations = total_data$official, color = "grey"
+  ) %>%
+  layout(title = "Official Percent Estimate", geo = g)
