6486d51c69d5088108cf4812381bb5b5318d0558
Valerie Chan <val7898@gmail.com>
2018-05-29 13:05:21 -0700

    Finished barplots and dotplot

--- scripts/dot_plot.R
+++ scripts/dot_plot.R
@@ -5,19 +5,30 @@ library("dplyr")
 data <- read.csv("../data/spm_2016.csv")
 View(data)
 
-factor(data$Official.Percent.Estimate)
+m <- list(
+  l = 125,
+  r = 50,
+  b = 100,
+  t = 100,
+  pad = 4
+)
 
-plot_ly(data, x = factor(data$Official.Percent.Estimate),  y = factor(data$State),
-        name = "Office % Estimate", type = 'scatter',
-             mode = "markers", marker = list(color = "pink")) %>%
+data$Official.Number.Estimate <- factor(data$Official.Percent.Estimate)
+s <- seq(0, 25, by = 5)
+
+plot_ly(x = seq(0, 25, by = 5),  y = factor(data$State)) %>% 
   add_trace(x = factor(data$SPM.Percent.Estimate), y = factor(data$State),
             name = "SPM % Estimate",type = 'scatter',
             mode = "markers", marker = list(color = "blue")) %>%
+  add_trace(x = factor(data$Official.Percent.Estimate), y = factor(data$State),
+            name = "Official % Estimate",type = 'scatter',
+            mode = "markers", marker = list(color = "red")) %>%
   layout(
-    autosize = F,
     title = "Poverty level measurements 2016",
-    xaxis = list(title = "State")
-   # margin = list(l = 100)
+    xaxis = list( title = "% of Poverty",
+                  showticklabels = TRUE,
+                  tickangle = 45),
+    margin = m
   )
 
 
